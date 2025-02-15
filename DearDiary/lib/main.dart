c5a7aa608473cd7859f9276baf8f8431c50826c9
Arnob <arnobg2002@gmail.com>
2025-02-15 18:25:25 -0330

    many pub dev shitz

--- DearDiary/lib/main.dart
+++ DearDiary/lib/main.dart
@@ -1,5 +1,11 @@
+import 'dart:io';
+
+import 'package:collection/collection.dart';
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/hive_flutter.dart';
+import 'package:open_file/open_file.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:pdf/widgets.dart' as pw;
 
 import '../controllers/car_controller.dart';
 import 'models/car_model.dart';
@@ -154,6 +160,13 @@ class _MyHomePageState extends State<MyHomePage> {
           return ListTile(
             title: Column(children: () {
               return [
+                if (index == 0)
+                  ElevatedButton(
+                    onPressed: () {
+                      DiaryPDFExporter.generatePDF(cars);
+                    },
+                    child: Text("Download as PDF"),
+                  ),
                 if (index == 0 ||
                     DateTime.parse(car.date).month !=
                         DateTime.parse(cars[index - 1].date).month)
@@ -241,6 +254,66 @@ class _MyHomePageState extends State<MyHomePage> {
         child: const Icon(Icons.add),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class DiaryPDFExporter {
+  static Future<void> generatePDF(List<CarModel> entries) async {
+    List<String> monthsInAYear = [
+      '',
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December"
+    ];
+    List<String> daysInAWeek = [
+      '',
+      'Monday',
+      'Tuesday',
+      'Wednesday',
+      'Thursday',
+      'Friday',
+      'Saturday',
+      'Sunday'
+    ];
+    final pdf = pw.Document();
+
+    pdf.addPage(
+      pw.Page(
+        build: (pw.Context context) => pw.Column(
+          crossAxisAlignment: pw.CrossAxisAlignment.center,
+          children: entries
+              .mapIndexed((i, entry) => pw.Column(children: [
+                    if (i == 0 ||
+                        DateTime.parse(entry.date).month !=
+                            DateTime.parse(entries[i - 1].date).month)
+                      pw.Text(
+                          '${monthsInAYear[DateTime.parse(entry.date).month]} ${DateTime.parse(entry.date).year}'),
+                    pw.Text(
+                        "${daysInAWeek[DateTime.parse(entry.date).weekday]}, ${monthsInAYear[DateTime.parse(entry.date).month]} ${DateTime.parse(entry.date).day}"),
+                    pw.Align(
+                        alignment: pw.Alignment.topLeft,
+                        child: pw.Text(entry.description))
+                  ]))
+              .toList(),
+        ),
+      ),
+    );
+
+    final directory = await getApplicationDocumentsDirectory();
+    final filePath = '${directory.path}/diary_entries.pdf';
+    final file = File(filePath);
+
+    await file.writeAsBytes(await pdf.save());
+    OpenFile.open(filePath);
   }
 }
 
--- DearDiary/lib/models/car_model.dart
+++ DearDiary/lib/models/car_model.dart
@@ -5,7 +5,7 @@ part 'car_model.g.dart';
 @HiveType(typeId: 0)
 class CarModel extends HiveObject {
   @HiveField(0)
-  String? description;
+  String description;
 
   @HiveField(1)
   String date;
@@ -13,5 +13,5 @@ class CarModel extends HiveObject {
   @HiveField(2)
   double rate;
 
-  CarModel({this.description, required this.rate, required this.date});
+  CarModel({required this.description, required this.rate, required this.date});
 }
